5c543c61245a6b7b30b8467d3385c37e75a9f971
zl <986023997@qq.com>
2020-04-20 21:12:29 +0800

    选择排序

--- sort/src/selectionsort/SelectionSort.java
+++ sort/src/selectionsort/SelectionSort.java
@@ -1,8 +1,10 @@
 package selectionsort;
 
+import java.util.Arrays;
+
 /**
  * @ClassNmae: SelectionSort
- * @Description: java类作用描述
+ * @Description: 选择排序：每一次循环找到最小数的下标并记录，结束后将最小值和最开始的基准值进行交换，与冒泡相比只需要交换一次
  * @Author: zhulin
  * @CreateDate: 2020/4/20$ 19:23$
  * @UpdateUser: zhulin
@@ -11,4 +13,23 @@ package selectionsort;
  * @Version: 1.0
  */
 public class SelectionSort {
+    public static int count=0;
+    public static void  selectionSort(int a[]){
+        for(int i=0;i<a.length-1;i++){//外循环控制循环的趟数
+            int min=i;
+            for(int j=i+1;j<a.length;j++){//内循环控制比较的次数
+                if(a[j]<a[min]){
+                    min=j;
+                }
+            }
+            int temp=a[min];
+            a[min]=a[i];
+            a[i]=temp;
+            ++count;
+            System.out.print("第"+count+"趟排序：");
+            System.out.println(Arrays.toString(a));
+        }
+
+    }
+
 }
